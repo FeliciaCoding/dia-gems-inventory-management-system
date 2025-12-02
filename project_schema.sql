5f48da656d50ed5b592ab68e98d1c68debb0620c
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2025-12-02 18:52:41 +0100

    Delete returned_qty (assum always 1) + create predefined types + fix employee_counterpart + fix employee_action

--- project_schema.sql
+++ project_schema.sql
@@ -4,7 +4,7 @@ SET search_path TO project;
 
 BEGIN;
 
-
+CREATE TYPE code AS ENUM ('USD', 'HKD', 'CHF', 'EUR', 'NTD');
 CREATE TABLE currency
 (
    code TEXT PRIMARY KEY,
@@ -24,7 +24,7 @@ CREATE TABLE counterpart
    email          TEXT UNIQUE
 );
 
-
+CREATE TYPE category AS ENUM ('Supplier', 'Client', 'Office', 'Labo', 'Manufactor');
 CREATE TABLE account_type
 (
    type_name   TEXT PRIMARY KEY,
@@ -43,15 +43,17 @@ CREATE TABLE counterpart_account_type
 
 );
 
-
+CREATE TYPE role AS ENUM ('Chief', 'Admin', 'Sales', 'Accountant');
 CREATE TABLE employee
 (
-   employee_id SERIAL PRIMARY KEY,
-   first_name  TEXT        NOT NULL,
-   last_name   TEXT        NOT NULL,
-   email       TEXT UNIQUE NOT NULL,
-   role        TEXT,
-   is_active   BOOLEAN     NOT NULL DEFAULT TRUE
+   employee_id    SERIAL PRIMARY KEY,
+   counterpart_id SERIAL      NOT NULL,
+   first_name     TEXT        NOT NULL,
+   last_name      TEXT        NOT NULL,
+   email          TEXT UNIQUE NOT NULL,
+   role           role,
+   is_active      BOOLEAN     NOT NULL DEFAULT TRUE,
+   FOREIGN KEY (counterpart_id) REFERENCES counterpart (counterpart_id)
 );
 
 
@@ -63,6 +65,15 @@ CREATE TABLE action
    creation_date TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
    last_update   TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL
       CHECK (last_update >= creation_date)
+);
+
+CREATE TABLE employee_action
+(
+   employee_id SERIAL,
+   action_id   SERIAL,
+   PRIMARY KEY (employee_id, action_id),
+   FOREIGN KEY (employee_id) REFERENCES employee (employee_id),
+   FOREIGN KEY (action_id) REFERENCES action (action_id)
 );
 
 
@@ -95,10 +106,10 @@ CREATE TABLE action_item
 (
    action_id     SERIAL,
    lot_id        SERIAL,
-   line_no       INT  NOT NULL,
-   qty           INT  NOT NULL,
-   unit_price    INT  NOT NULL,
-   currency_code TEXT NOT NULL,
+   line_no       INT           NOT NULL,
+   qty           INT           NOT NULL,
+   unit_price    DECIMAL(10.2) NOT NULL,
+   currency_code TEXT          NOT NULL,
    PRIMARY KEY (action_id, lot_id),
    UNIQUE (action_id, line_no),
    FOREIGN KEY (action_id) REFERENCES action (action_id),
@@ -143,11 +154,9 @@ CREATE TABLE return_memo_in_items
 (
    action_id    SERIAL,
    lot_id       SERIAL,
-   qty_returned INT NOT NULL,
    PRIMARY KEY (action_id, lot_id),
    FOREIGN KEY (action_id) REFERENCES return_memo_in (action_id),
    FOREIGN KEY (lot_id) REFERENCES item (lot_id),
-   CHECK (qty_returned > 0)
 );
 
 
@@ -175,11 +184,9 @@ CREATE TABLE return_memo_out_items
 (
    return_action_id SERIAL,
    lot_id           SERIAL NOT NULL,
-   qty_returned     INT    NOT NULL,
    PRIMARY KEY (return_action_id, lot_id),
    FOREIGN KEY (return_action_id) REFERENCES return_memo_out (action_id),
-   FOREIGN KEY (lot_id) REFERENCES item (lot_id),
-   CHECK (qty_returned > 0)
+   FOREIGN KEY (lot_id) REFERENCES item (lot_id)
 );
 
 
@@ -216,7 +223,6 @@ CREATE TABLE back_from_lab_items
 (
    action_id    SERIAL,
    lot_id       SERIAL NOT NULL,
-   qty_returned INT    NOT NULL,
    PRIMARY KEY (action_id, lot_id),
    FOREIGN KEY (action_id) REFERENCES back_from_lab (action_id),
    FOREIGN KEY (lot_id) REFERENCES item (lot_id)
@@ -243,14 +249,22 @@ CREATE TABLE back_from_factory
    FOREIGN KEY (orig_transfer_id) REFERENCES transfer_to_factory (action_id)
 );
 
-
+-- QUESTION :  can i update the items using FK? how to update the item after being repolished ?
 CREATE TABLE back_from_factory_details
 (
-   action_id    SERIAL PRIMARY KEY,
-   lot_id       SERIAL NOT NULL,
-   qty_returned INT    NOT NULL,
+   action_id       SERIAL PRIMARY KEY,
+   lot_id          SERIAL NOT NULL,
+   after_weight_ct DECIMAL(10, 2),
+   after_shape     shape,
+   after_length    DECIMAL(10, 2),
+   after_width     DECIMAL(10, 2),
+   after_depth     DECIMAL(10, 2),
    FOREIGN KEY (action_id) REFERENCES back_from_factory (action_id),
-   FOREIGN KEY (lot_id) REFERENCES item (lot_id)
+   FOREIGN KEY (lot_id) REFERENCES item (lot_id),
+   FOREIGN KEY (after_shape)  REFERENCES loose_stone (shape),
+   FOREIGN KEY (after_length)  REFERENCES loose_stone (length),
+   FOREIGN KEY (after_width)  REFERENCES loose_stone (width),
+   FOREIGN KEY (after_depth)  REFERENCES loose_stone (depth)
 );
 
 CREATE TABLE sale
@@ -281,10 +295,13 @@ CREATE TABLE item
 
 -- loose_stone (**lot_id**, weight_ct, length, width, depth)
 --    loose_stone.lot_id references item.lot_id
+
+CREATE TYPE shape AS ENUM ('Brilliant Cut', 'Pear Shape', 'Radiant Cut', 'Heart Shape', 'Emerald Cut', 'Baquette', 'Briolette', 'Kite', 'Marquise', 'Oval', 'Princess', 'Trillion');
 CREATE TABLE loose_stone
 (
    lot_id    SERIAL PRIMARY KEY,
    weight_ct DECIMAL(10, 2) NOT NULL,
+   shape     shape          NOT NULL,
    length    DECIMAL(10, 2) NOT NULL,
    width     DECIMAL(10, 2) NOT NULL,
    depth     DECIMAL(10, 2) NOT NULL,
@@ -293,49 +310,58 @@ CREATE TABLE loose_stone
 
 -- white_diamond (**lot_id**, white_level, shape, clarity)
 --     white_diamond.lot_id references loose_stone.lot_id
+
+CREATE TYPE clarity AS ENUM ('I1', 'I2', 'VS', 'VS1', 'VS2', 'VVS', 'VVS1', 'VVS2','FL', 'IF');
 CREATE TABLE white_diamond
 (
    lot_id      SERIAL PRIMARY KEY,
    white_level INTEGER NOT NULL,
-   shape       TEXT    NOT NULL,
-   clarity     TEXT    NOT NULL,
+   clarity     clarity NOT NULL,
    FOREIGN KEY (lot_id) REFERENCES loose_stone (lot_id)
 );
 
 -- colored_diamond (**lot_id**, gem_type, fancy_intensity, fancy_overton, fancy_color, shape, clarity)
 --     colored_diamond.lot_id references loose_stone.lot_id
+
+CREATE TYPE gem_type AS ENUM ('Sapphire', 'Emerald', 'Ruby', 'Diamond');
+CREATE TYPE fancy_intensity AS ENUM ('Faint', 'Very Light', 'Light', 'Fancy light', 'Fancy','Fansy Vivid', 'Fancy intense', 'Fancy Deep', 'Fansy Dark');
+CREATE TYPE fancy_color AS ENUM ('Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Violet', 'Gray');
+
 CREATE TABLE colored_diamond
 (
    lot_id          SERIAL PRIMARY KEY,
-   gem_type        TEXT NOT NULL,
-   fancy_intensity TEXT NOT NULL,
-   fancy_overton   TEXT NOT NULL,
-   fancy_color     TEXT NOT NULL,
-   shape           TEXT NOT NULL,
-   white_level     TEXT NOT NULL,
-   clarity         TEXT NOT NULL,
+   gem_type        TEXT            NOT NULL,
+   fancy_intensity fancy_intensity NOT NULL,
+   fancy_overtone  TEXT            NOT NULL,
+   fancy_color     TEXT            NOT NULL,
+   clarity         clarity         NOT NULL,
    FOREIGN KEY (lot_id) REFERENCES loose_stone (lot_id)
 );
 
 -- colored_gem_stone (**lot_id**, gem_type, shape, color, treatment, origin)
 --     colored_gem_stone.lot_id references loose_stone.lot_id
+
+CREATE TYPE treatment AS ENUM ('No heat', 'heated', 'No oil', 'Minor Oil', 'Oiled');
+CREATE TYPE gem_color AS ENUM ('Red', 'Blue', 'Green', 'Pigeon blood', 'Royal Blue');
 CREATE TABLE colored_gem_stone
 (
    lot_id    SERIAL PRIMARY KEY,
-   gem_type  TEXT NOT NULL,
-   shape     TEXT NOT NULL,
-   color     TEXT NOT NULL,
-   treatment TEXT NOT NULL,
+   gem_type  gem_type  NOT NULL,
+   shape     shape     NOT NULL,
+   gem_color gem_color NOT NULL,
+   treatment treatment NOT NULL,
    FOREIGN KEY (lot_id) REFERENCES loose_stone (lot_id)
 );
 
 -- jewerly (**lot_id**, jew_type, gross_weight_gr, metal_type, metal_weight_gr,
 --     total_center_stone_qty, total_center_stone_weight_ct, centered_stone_type,
 --    total_side_stone_qty, total_side_stone_weight_ct, side_stone_type)
+CREATE TYPE jewelry_type AS ENUM ('Earrings', 'Necklace', 'Ring', 'Brooch', 'Bracelet');
+CREATE TYPE metal_type AS ENUM ('PT900', 'PT950', '18k white gold', '14k white gold', '18k white/yellow gold', '18k rose gold', '18k white gold + PT');
 CREATE TABLE jewelry
 (
    lot_id                     SERIAL PRIMARY KEY,
-   jewelry_type               TEXT           NOT NULL,
+   jewelry_type               jewelry_type   NOT NULL,
    gross_weight_gr            DECIMAL(10, 2) NOT NULL,
    metal_type                 TEXT           NOT NULL,
    metal_weight_gr            DECIMAL(10, 2) NOT NULL,
@@ -353,15 +379,15 @@ CREATE TABLE certificate
    lab_id          SERIAL                                 NOT NULL,
    certificate_num TEXT                                   NOT NULL,
    issue_date      TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
-   shape           TEXT,
+   shape           shape,
    weight_ct       DECIMAL(10, 2),
    length          DECIMAL(10, 2),
    width           DECIMAL(10, 2),
    depth           DECIMAL(10, 2),
-   clarity         TEXT,
-   color           TEXT,
-   treatment       TEXT,
-   gem_type        TEXT,
+   clarity         clarity,
+   color           fancy_color,
+   treatment       treatment,
+   gem_type        gem_type,
    creation_date   TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
    last_update     TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
 
