4072ed3d6f5367d94ba4fd76f0ef6793a8e09476
maxmakovskiy <makovskiymax@gmail.com>
2025-12-31 22:17:19 +0100

    feat: [inventory-pages] trying to blend purchase and memo-in

---
+++ src/diamonds_ui/database/action/memo_in.py
@@ -0,0 +1,55 @@
+from decimal import Decimal
+from datetime import date, datetime
+from pydantic import BaseModel
+import psycopg
+from psycopg.rows import class_row
+
+
+class MemoIn(BaseModel):
+    action_id: int
+    from_counterpart_name: str
+    to_counterpart_name: str
+    terms: str
+    remarks: str
+    price: Decimal
+    currency_code: str
+    created_at: datetime
+    updated_at: datetime
+    memo_in_num: str
+    ship_date: date
+    expected_return_date: date | None
+
+
+def get_memo_in(
+        db: psycopg.Connection,
+        lot_id: int
+):
+    with db.cursor(row_factory=class_row(MemoIn)) as cur:
+        return cur.execute(
+            """
+            SELECT a.action_id,
+                c1.name AS from_counterpart_name,
+                c2.name AS to_counterpart_name,
+                terms,
+                remarks,
+                (ai.unit_price * ai.quantity) AS price,
+                currency_code,
+                a.created_at,
+                a.updated_at,
+                memo_in_num,
+                ship_date,
+                expected_return_date
+            FROM diamonds_are_forever.action_item ai
+                INNER JOIN diamonds_are_forever.action a
+                ON ai.action_id = a.action_id
+                INNER JOIN diamonds_are_forever.memo_in mi
+                ON mi.action_id = a.action_id
+                INNER JOIN diamonds_are_forever.counterpart c1
+                ON a.from_counterpart_id = c1.counterpart_id
+                INNER JOIN diamonds_are_forever.counterpart c2
+                ON a.to_counterpart_id = c2.counterpart_id
+            WHERE ai.lot_id = %s
+            """,
+            (lot_id,),
+        ).fetchone()
+
--- src/diamonds_ui/pages/inventory/inventory_white_diamonds.py
+++ src/diamonds_ui/pages/inventory/inventory_white_diamonds.py
@@ -11,26 +11,39 @@ from diamonds_ui.auth import user
 from diamonds_ui.components.pagination import pagination_element
 from diamonds_ui.database.item.white_diamond import WhiteDiamond, white_diamonds_cursor, get_white_diamond
 from diamonds_ui.database.action.purchase import Purchase, get_purchase
+from diamonds_ui.database.action.memo_in import MemoIn, get_memo_in
 from streamlit_utils import db
 
 
-def render_white_diamond_details(d: WhiteDiamond, *, purchase = None):
+def render_white_diamond_details(d: WhiteDiamond, incoming: Purchase | MemoIn):
     with st.container(border=True):
         st.markdown(f"### Details for: {d.stock_name}")
         st.caption(f"Lot id: #{d.lot_id}")
 
+        st.markdown(f"**Current location:** {d.physical_location}")
         st.markdown(f"**Weight:** {d.weight_ct} ct")
         st.markdown(f"**Shape:** {d.shape}")
         st.markdown(f"**White scale:** {d.white_scale}")
         st.markdown(f"**Certificate:** {d.certificate_num}")
 
         st.markdown(f"#### Status")
-        if purchase is not None:
-            with st.container(border=True):
-                st.markdown(f"#### Purchase:")
-                st.markdown(f"**From:** {purchase.from_counterpart_name}. **To:** {purchase.to_counterpart_name}")
-                st.markdown(f"**Price:** {purchase.price} {purchase.currency_code}")
-                st.markdown(f"**Purchase date:** {purchase.purchase_date}")
+
+        match incoming:
+            case Purchase():
+                with st.container(border=True):
+                    st.markdown(f"#### Purchase:")
+                    st.markdown(f"**From:** {incoming.from_counterpart_name}. **To:** {incoming.to_counterpart_name}")
+                    st.markdown(f"**Price:** {incoming.price} {incoming.currency_code}")
+                    st.markdown(f"**Purchase date:** {incoming.purchase_date}")
+            case MemoIn():
+                st.markdown(f"#### Memo In:")
+                st.markdown(f"**From:** {incoming.from_counterpart_name}. **To:** {incoming.to_counterpart_name}")
+                st.markdown(f"**Price:** {incoming.price} {incoming.currency_code}")
+                st.markdown(f"**Memo in number:** {incoming.memo_in_num}")
+                st.markdown(f"**Ship date:** {incoming.ship_date}")
+                if incoming.expected_return_date is not None:
+                    st.markdown(f"**Expected return date:** {incoming.expected_return_date}")
+
 
     if st.button("Back to list"):
         st.session_state.selected_lot_id = None
@@ -71,7 +84,7 @@ else:
         if st.session_state[_SELECTED_LOT_ID_KEY] is not None:
             wd = get_white_diamond(db, st.session_state[_SELECTED_LOT_ID_KEY])
             purchase = get_purchase(db, wd.lot_id)
-            render_white_diamond_details(wd, purchase=purchase)
+            render_white_diamond_details(wd, purchase)
         else:
             with white_diamonds_cursor(
                 db,
