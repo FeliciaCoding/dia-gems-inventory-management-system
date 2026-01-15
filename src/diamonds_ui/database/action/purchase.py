f20274622f0259983cdd6ae76a7ee3aebc4d754a
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2026-01-15 15:40:11 +0100

    Add outlines for create_purchase_white_diamonds - WIP

--- src/diamonds_ui/database/action/purchase.py
+++ src/diamonds_ui/database/action/purchase.py
@@ -11,6 +11,39 @@ class Purchase(BaseModel):
     purchase_num: str
     purchase_date: date
 
+#
+def create_purchase_white_diamonds(
+        db: psycopg.Connection,
+        employee_id: int,
+        stock_name: str,
+        purchase_date: date,
+        purchase_num: str,
+        origin: str,
+        supplier_id: int,
+        office_id: int,
+        price: Decimal,
+        currency: str,
+        weight_ct: Decimal,
+        shape: str,
+        length: Decimal,
+        width: Decimal,
+        depth: Decimal,
+        white_scale: str,
+        clarity: str
+ # return lot id for new stone
+) -> int :
+    with db.cursor() as cur:
+        cur.execute("SET search_path TO diamonds_are_forever")
+        # add into action
+        # add into item
+        # add into loose stone
+        # add white diamond
+        # add action_item
+        # add purcahse
+        # add log
+
+        # commit and return
+
 
 def get_purchases(
         db: psycopg.Connection,
@@ -31,4 +64,3 @@ def get_purchases(
         )
         cur.execute("SET search_path TO diamonds_are_forever")
         return cur.execute(q, other_params).fetchall()
-
