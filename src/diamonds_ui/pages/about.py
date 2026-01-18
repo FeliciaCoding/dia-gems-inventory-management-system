b9ddec422b1c937a8e653ff3c6b91f9ceb070432
maxmakovskiy <makovskiymax@gmail.com>
2026-01-18 21:54:55 +0100

    adding some info to the about page

--- src/diamonds_ui/pages/about.py
+++ src/diamonds_ui/pages/about.py
@@ -4,9 +4,16 @@ and its creators
 """
 
 import streamlit as st
-# from psycopg import sql
 
-st.header("About us")
+st.header("About")
 
+with st.container(border=True):
+    st.markdown("#### Diamonds")
+    st.write("Developed system is a secure inventory management and traceability platform dedicated to diamonds, colored stones, and jewelry. ")
+    st.write("It acts as a centralized and reliable source of information for identifying, tracking, and managing goods throughout their lifecycle.")
 
-
+with st.container(border=True):
+    st.markdown("#### Contributors")
+    st.write("- Liao Pei-Wen")
+    st.write("- Makovskyi Maksym")
+    st.write("- Wu Guo Yu")
