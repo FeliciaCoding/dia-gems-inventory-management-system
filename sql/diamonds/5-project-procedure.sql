050067682b52006d3000ccd44b72414978ff94af
FeliciaCoding <49447678+FeliciaCoding@users.noreply.github.com>
2026-01-18 04:06:38 +0100

    Seperate test cases to another file

--- sql/diamonds/5-project-procedure.sql
+++ sql/diamonds/5-project-procedure.sql
@@ -45,23 +45,7 @@ END;
 $$;
 -- END PROCEDURE #1
 
-CALL pcd_create_purchase(
-    5,
-    1,
-    'PO-TEST-0001',
-    'TEST-WD-0001',
-    'South Africa',
-    'white diamond'::item_category,
-    18000.00,
-    'USD'::code
-);
 
-SELECT *
-FROM purchase p
-JOIN action a USING (action_id)
-JOIN action_item ai USING (action_id)
-ORDER BY p.action_id DESC
-LIMIT 1;
 
 
 -- BEGIN PROCEDURE #2
@@ -98,24 +82,7 @@ END;
 $$;
 -- END PROCEDURE #2
 
-CALL pcd_create_memo_out(
-    1,
-    13,
-    'MO-TEST-0001',
-    CURRENT_DATE,
-    (CURRENT_DATE + 14)::DATE,
-    42,
-    35000.00,
-    'USD'::code
-);
 
-
-SELECT *
-FROM memo_out m
-JOIN action a USING (action_id)
-JOIN action_item ai USING (action_id)
-ORDER BY m.action_id DESC
-LIMIT 1;
 
 -- BEGIN PROCEDURE #3
 -- Create new sale :
@@ -150,22 +117,3 @@ BEGIN
 END;
 $$;
 -- END PROCEDURE #3
-
-CALL pcd_create_sale(
-    2,
-    15,
-    'SO-TEST-001',
-    'Wire Transfer',
-    'Unpaid'::payment_status,
-    34,
-    52000.00,
-    'USD'::code
-);
-
-
-SELECT *
-FROM diamonds_are_forever.sale s
-JOIN diamonds_are_forever.action a USING (action_id)
-JOIN diamonds_are_forever.action_item ai USING (action_id)
-ORDER BY s.action_id DESC
-LIMIT 1;
